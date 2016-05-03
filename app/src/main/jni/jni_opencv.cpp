6d2dcaba188115d75eaf5616559bdad10543ada2
ArcSung <ArcSung@via.com.tw>
2016-05-03 14:01:29 +0800

    remove some not useful code

--- app/src/main/jni/jni_opencv.cpp
+++ app/src/main/jni/jni_opencv.cpp
@@ -1,10 +1,8 @@
 #include <jni.h>
 #include <opencv2/core/core.hpp>
 #include <opencv2/imgproc/imgproc.hpp>
-#include <opencv2/imgproc/imgproc_c.h>
 #include <opencv2/features2d/features2d.hpp>
 #include <opencv2/calib3d.hpp>
-#include <vector>
 #include <android/log.h>
 #include "myCVClasses.hpp"
 
@@ -26,10 +24,7 @@ Size imageSize(CAP_WIDTH, CAP_HEIGHT);
 const int CUBE_SIZE = 5;
 const int CHESS_SIZE = 25;
 
-
-std::vector<cv::Point2f> corners;								//Vector which will contain the corner coordinates for each camera frame
-std::vector<cv::Point3f> _3DPoints;								//Vector that contains the 3D coordinates for each chessboard corner
-vector<Point3f> objectCorners, contoursFindedObjectPoints;
+vector<Point3f> contoursFindedObjectPoints;
 vector< vector<Point2f> > contoursFinded;
 myCV::CameraCalibration *camCalib;
 Mat rvecs, tvecs;
@@ -88,13 +83,6 @@ extern "C"
 
 inline double dist(Point a, Point b){
         return sqrt( (a.x-b.x) * (a.x-b.x) + (a.y-b.y) * (a.y-b.y) );
-    }
-
-    String int2str(int i) {
-         char* s;
-         sprintf(s,"%d",i);
-
-        return (string)s;
     }
 
     void drawMarkerContours(Mat image, Mat mgray){
@@ -249,22 +237,7 @@ inline double dist(Point a, Point b){
     Java_org_opencv_samples_cameracalibration_CameraCalibrationActivity_Init(JNIEnv * , jobject)
     {
         //Initialising the 3D-Points for the chessboard
-        float rot = 0.0f;
-        float a = 0.2f;								//The widht/height of each square of the chessboard object
-        Point3f _3DPoint;
-        float y = (((boardSize.height-1.0f)/2.0f)*a)+(a/2.0f);
-        float x = 0.0f;
-        /*for(int h = 0; h < boardSize.height; h++, y+=a){
-            x = (((boardSize.width-2.0f)/2.0f)*(-a))-(a/2.0f);
-            for(int w = 0; w < boardSize.width; w++, x+=a){
-                _3DPoint.x = x;
-                _3DPoint.y = y;
-                _3DPoint.z = 0.0f;
-                _3DPoints.push_back(_3DPoint);
-            }
-        }*/
         camCalib = new myCV::CameraCalibration(boardSize.width, boardSize.height, nImages);
-        //camCalib(boardSize.width, boardSize.height, nImages);		//create an object which handels the camera calibration
         camCalib->Initialisation();
 
         int x2 = 100;
@@ -303,11 +276,6 @@ inline double dist(Point a, Point b){
         Mat& mGr  = *(Mat*)addrGray;
         Mat& mRgb = *(Mat*)addrRgba;
 
-        //The size of the chessboard which should be searched during the camera calibration plus
-        //the number of images which should be taken
-
-        bool found = false;								//Will be used to determine wether a chessboard has been
-        //found in the camera frame or not
         if(!done) {
             camCalib->GrabFrames(mGr, mRgb);
 
@@ -344,16 +312,11 @@ inline double dist(Point a, Point b){
 
                 Mat m1(contoursFinded[0]);
                 Mat m2(contoursFindedObjectPoints);
-                CvPoint2D32f srcTri[4], dstTri[4];
-
 
                 int ROIW = brx - tlx - 1;
                 int ROIH = bry - tly - 1;
 
-
                 Mat SrcROI = mRgb(Rect(tlx, tly, ROIW, ROIH));
-                //SrcROI.type() = mRgb.type();
-                //Mat SrcROI(ROIW, ROIH, mRgb.type(), Scalar(255, 255, 255));
 
                 // 設定變換[之前]與[之後]的坐標 (左上,左下,右下,右上)
                 const int nOffset=200;
@@ -431,9 +394,6 @@ inline double dist(Point a, Point b){
                 if(ROIW < mRgb.rows/2 && ROIH < mRgb.cols/2) {
                     LOGI("ROI %d, %d", ROIW, ROIH);
                     dst_img.copyTo(mRgb(Rect(0, 0, ROIW, ROIH)));
-                    //mRgb.copyTo(dst_img);
-                    //imwrite("/sdcard/CameraCalib.jpg", SrcROI);
-                    //imwrite("/sdcard/CameraCalib2.jpg", dst_img);
                 }
 
                 m1.release();
@@ -447,39 +407,6 @@ inline double dist(Point a, Point b){
 
         }
     }
-
-                /*corners.clear();
-
-                cv::Mat M = camCalib->getIntrinsicsMatrix();
-                cv::Mat D = camCalib->getDistortionCoeffs();
-
-
-                //Search for a chessboard in the camera frame and save his corners
-                found = cv::findChessboardCorners(mGr, boardSize, corners);
-                LOGI("found:%d", found);
-                if(found)
-                { //A chessboard has been found
-                    cv::cornerSubPix(mGr, corners, cv::Size(5, 5), cv::Size(-1,-1), cv::TermCriteria(CV_TERMCRIT_EPS+CV_TERMCRIT_ITER, 30, 0.1 ));
-                    cv::drawChessboardCorners(mRgb, boardSize, cv::Mat(corners), found);	//Draw the chessboard corners on the camera frame
-
-                    cv::solvePnP(cv::Mat(_3DPoints), cv::Mat(corners), M, D, R, T);		//Calculate the Rotation and Translation vector
-                    double theta = 0;
-                    //Calculate the angle of the rotation
-                    theta = sqrt((R.at<double>(0,0)*R.at<double>(0,0))+
-                         (R.at<double>(1,0)*R.at<double>(1,0))+
-                         (R.at<double>(2,0)*R.at<double>(2,0)));
-                    corners.clear();
-                   // LOGI("found a");
-
-                    //Vec3d eulerAngles;
-                    //getEulerAngles(R,eulerAngles);
-                  //  LOGI("found b");
-
-                    LOGI("theta:%d, R1: %d, R2: %d, R3: %d", (theta*180.0f)/3.14159f, R.at<double>(0,0), R.at<double>(1,0), R.at<double>(2,0));
-                    //LOGI("T1: %d, T2: %d, T3: %d",  -T.at<double>(0,0) , (-T.at<double>(1,0)-1.0), (-T.at<double>(2,0)+5.0));
-                   // LOGI("yaw: %d, pitch: %d, roll: %d",  eulerAngles[1] , eulerAngles[0], eulerAngles[2]);
-
-                }*/
 
 }
 
